978d6191d10fcf5707d696ea06f376949478b640
shenghung <shenghung619@gmail.com>
2022-12-22 02:11:46 +0800

    DB連好了！！！！！

--- SmokerKiller/src/Main.java
+++ SmokerKiller/src/Main.java
@@ -2,6 +2,21 @@
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			Connection connection= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sadd","root","12345678");
+			System.out.println("Connected successfully");
+			Shop shop = new Shop();
+			shop.init();
+			LoginPage lgPage = new LoginPage(shop);
+			lgPage.setVisible(true);
+		}
+
+
+		}catch(SQLException e) {
+			System.out.println("Error while connecting");
+			e.printStackTrace();
+		}
+		
 		Shop shop = new Shop();
 		shop.init();
 		LoginPage lgPage = new LoginPage(shop);
